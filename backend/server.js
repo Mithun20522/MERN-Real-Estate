12325c5cf9afce41e8d27b7e4090dd03f49db7fa
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-20 16:01:49 +0530

    server initialized and mongodb connected.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log('backend')
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookies from 'cookie-parser';
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
+
+const app = express();
+
+app.use(express.json());
+app.use(cookies());
+
+mongoose.connect(MONGO_DB_URL)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+})
